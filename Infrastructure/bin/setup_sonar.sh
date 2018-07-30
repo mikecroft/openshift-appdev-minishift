7f31c82a3e429052a51903ce55c54b1494b4bdd3
Mike Croft <backtotheoldhouse@gmail.com>
2018-07-30 10:35:24 +0100

    add postgres for sonarqube

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -14,3 +14,13 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
+
+
+# Postgres
+oc new-app
+    --template=postgresql-persistent \
+    --param POSTGRESQL_USER=sonar \
+    --param POSTGRESQL_PASSWORD=sonar \
+    --param POSTGRESQL_DATABASE=sonar \
+    --param VOLUME_CAPACITY=4Gi \
+    --labels=app=sonarqube
